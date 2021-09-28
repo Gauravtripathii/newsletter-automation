986b62efb3a3e34289764301cefb41fbdfd6b51d
Gauravtripathii <59873840+Gauravtripathii@users.noreply.github.com>
2021-09-28 14:05:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,28 @@
-from website import create_app
+from website import create_app, db
+from website.models import User
+from website.automation import send_email
+from flask_restful import Api, Resource
+import datetime
 
-if __name__=='__main__':
-	app=create_app()
-	app.run(debug=True)
+app=create_app()
+api=Api(app)
+
+class SendingData(Resource):
+	def get(self):
+		emails = User.query.all()
+		content = ''
+		now = datetime.datetime.now()
+
+		email_list = [E.email for E in emails]
+		print(email_list)
+
+		send_email(content, now, email_list)
+		print('sent')
+
+		return {'emails':[email_list]}
+
+
+api.add_resource(SendingData,'/all-emails')
+
+
+app.run(debug=True)
